8cb1eb9f89be8675f8160fd1638ebe1fd811877f
Mohamed Blgikey <a.z29620014@gmail.com>
2022-06-22 01:40:54 +0200

    Finish

--- src/app/chat/conv/conv.component.ts
+++ src/app/chat/conv/conv.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HubConnection } from '@aspnet/signalr';
+import * as signalr from '@aspnet/signalr'
 import { AuthService } from 'src/app/auth.service';
 import { HttpService } from 'src/app/http.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-conv',
   templateUrl: './conv.component.html',
   styleUrls: ['./conv.component.scss']
 })
-export class ConvComponent implements OnInit ,OnChanges{
+export class ConvComponent implements OnInit ,OnChanges,AfterViewChecked{
 
+  @ViewChild('panel') panel!:ElementRef;
   @Input() user!:any;
   con!:any[];
+  private HubConnection!:HubConnection;
 
   SendForm:FormGroup = new FormGroup(
     {
@@ -24,6 +29,10 @@ export class ConvComponent implements OnInit ,OnChanges{
 
 
   constructor(private http : HttpService,private auth:AuthService) { }
+  ngAfterViewChecked(): void {
+
+    this.panel.nativeElement.scrollTop = this.panel?.nativeElement.scrollHeight;
+  }
   ngOnChanges(): void {
     // console.log(this.user) ;
     if (this.user != null) {
@@ -38,6 +47,38 @@ export class ConvComponent implements OnInit ,OnChanges{
   }
 
   ngOnInit(): void {
+    this.HubConnection = new signalr.HubConnectionBuilder().withUrl(`${environment.baseUrl}/Notify`).build();
+    this.HubConnection
+    .start().then(()=>{
+      console.log("Start");
+    }).catch((err)=>{
+      console.log(err);
+    })
+    this.HubConnection
+    .onclose(()=>{
+      setTimeout(() => {
+        console.log("Restart");
+        this.HubConnection
+    .start().then(()=>{
+      console.log("Start");
+    }).catch((err)=>{
+      console.log(err);
+    })
+      }, 5000);
+    })
+
+
+    this.HubConnection.on("ReciveMessage",((data:any)=>{
+      if (this.auth.user['_value'].nameid == data.reciverId || this.auth.user['_value'].nameid == data.senderId) {
+        console.log(data);
+        this.http.Get(`/Users/GetConversation/${this.user?.id}`).subscribe(res=>{
+          this.con = res
+          // console.log(this.con);
+        })
+      }
+    }))
+
+
 
   }
 
